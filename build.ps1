6e3d05034fd15c64e0fd1b29cc63e39db2af354d
MohammadSaad2000 <saadsword@gmail.com>
2023-12-22 16:34:56 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,12 @@
+Remove-Item '.\build' -Recurse -Force
+
+Copy-Item -Path ".\icons" -Destination ".\build\yuzu_ssbu_launcher\icons" -Recurse -Force
+
 # get rsrc from https://github.com/akavel/rsrc
-cd ".\build"
-Copy-Item -Path "..\icons" -Destination ".\yuzu_ssbu_launcher" -Recurse -Force
-..\rsrc_windows_amd64.exe -ico "..\icons\ssbu_red.ico" -o ".\rsrc.syso"
-go build "..\" -o ".\yuzu_ssbu_launcher\yuzu_ssbu_launcher.exe"
-Compress-Archive -Path ".\yuzu_ssbu_launcher" -DestinationPath ".\yuzu_ssbu_launcher.zip" -Force
+.\rsrc_windows_amd64.exe -ico ".\icons\ssbu_red.ico" -o ".\rsrc.syso"
+
+go build -o ".\yuzu_ssbu_launcher.exe"
+
+Move-Item -Path ".\yuzu_ssbu_launcher.exe" -Destination ".\build\yuzu_ssbu_launcher"
+
+Compress-Archive -Path ".\build\yuzu_ssbu_launcher" -DestinationPath ".\build\yuzu_ssbu_launcher.zip" -Force
